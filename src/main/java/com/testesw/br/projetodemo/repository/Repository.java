726be32ad2224b8bd80af1e8478f6e984d1f73a5
emersomcarvalho <show.021@hotmail.com>
2019-06-13 02:51:49 -0300

    Configurado MongoDB

---
+++ src/main/java/com/testesw/br/projetodemo/repository/Repository.java
@@ -0,0 +1,10 @@
+package com.testesw.br.projetodemo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.testesw.br.projetodemo.entity.StarWorsEntity;
+
+
+public interface Repository extends MongoRepository<StarWorsEntity, Long>{
+
+}
