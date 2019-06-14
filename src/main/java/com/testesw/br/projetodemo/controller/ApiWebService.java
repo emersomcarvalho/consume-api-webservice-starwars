f4d01e33d9d272d4759a5ffb7818f8dc1f7e8e78
Emersom Carvalho <emersom.carvalho@cnseg.org.br>
2019-06-14 19:13:41 -0300

    Atualizando Métodos

--- src/main/java/com/testesw/br/projetodemo/controller/ApiWebService.java
+++ src/main/java/com/testesw/br/projetodemo/controller/ApiWebService.java
@@ -3,14 +3,11 @@ package com.testesw.br.projetodemo.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,16 +32,19 @@ public class ApiWebService {
     public Object getPlanets(@PathVariable("id") String id) throws Exception {
     	return swapiService.getPlanets(id);
     }
+    
     //add post
     @RequestMapping(value = "", method = RequestMethod.POST)			
     public StarWorsEntity starAdd(@RequestBody StarWorsEntity contato) {
     	return repository.save(contato);
     }
+    
     //add list
    	@GetMapping														
     public List<StarWorsEntity> listar(){
     	return repository.findAll();
     	}
+   	
      // buscar por id
      @RequestMapping("/{id}")				
       public ResponseEntity<StarWorsEntity> search(@PathVariable Long id){
@@ -62,6 +62,5 @@ public class ApiWebService {
     }
   
 
-  
 
 
